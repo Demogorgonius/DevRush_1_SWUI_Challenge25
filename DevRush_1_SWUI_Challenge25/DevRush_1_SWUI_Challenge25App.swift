d897558c964a48dc2e77f32f7a0c4ee1f692cf27
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2025-07-18 21:21:51 +0300

    Initial Commit

---
+++ DevRush_1_SWUI_Challenge25/DevRush_1_SWUI_Challenge25App.swift
@@ -0,0 +1,17 @@
+//
+//  DevRush_1_SWUI_Challenge25App.swift
+//  DevRush_1_SWUI_Challenge25
+//
+//  Created by Sergey on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DevRush_1_SWUI_Challenge25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevRush_1_SWUI_Challenge25Tests/DevRush_1_SWUI_Challenge25Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DevRush_1_SWUI_Challenge25Tests.swift
+//  DevRush_1_SWUI_Challenge25Tests
+//
+//  Created by Sergey on 18.07.2025.
+//
+
+import Testing
+@testable import DevRush_1_SWUI_Challenge25
+
+struct DevRush_1_SWUI_Challenge25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
